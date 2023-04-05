74e914dce4a8b86b72c1f1428b7e25b81e369ca2
Andy <andychc731@gmail.com>
2023-04-05 11:19:02 -0600

    Add solutions till test #3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let removeIndex = [...args].sort();
 
+    for (let i = removeIndex.length - 1; i >= 0; i--){
+        if (typeof removeIndex[i] === "number") {
+            array.splice(removeIndex[i] - 1, 1);
+        }
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
