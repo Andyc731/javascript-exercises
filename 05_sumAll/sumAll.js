c90082b7c4242f5570f601d54afc987fb2ca0e41
Andy <andychc731@gmail.com>
2023-04-05 12:02:12 -0600

    Add solutions to all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let final = 0;
+    if (num1 >= 0 && num2 >= 0 && typeof num1 === "number" && typeof num2 === "number") {
+        if (num1 < num2){
+            for (let i = num2; i >= num1; i--) {
+                final += i;
+            }
+        } else {
+            for (let i = num1; i >= num2; i--) {
+                final += i;
+            }
+        }
+    } else {
+        final = "ERROR";
+    }
+    return final;
 
 };
 
