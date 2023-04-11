6206d75dc8c8775b2c84b44d191c33bc12291264
Andy <andychc731@gmail.com>
2023-04-11 15:22:56 -0600

    Add solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+  let final = 0;
+	for (let key in num) {
+    final += num[key];
+  }
+  return final;
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  let final = 1;
+	for (let key in num) {
+    final *= num[key];
+  }
+  return final;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let final = 1;
+  for (let i = 0; i < num2; i++){
+    final *= num1;
+  }
+	return final;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	function factorial(num) {
+    if (num > 1) {
+      num *= factorial(num-1);
+      return num;
+    } else {
+      return 1;
+    }
+  }
+  return factorial(num);
 };
 
 // Do not edit below this line
