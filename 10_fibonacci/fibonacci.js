a170279264ddf063fd8f52c4dbc788bd3c5414b3
Andy <andychc731@gmail.com>
2023-04-12 11:23:27 -0600

    Add solutions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    function fibo(num) {
+        if (num > 2) {
+            num = fibo(num - 1) + fibo(num - 2);
+            return num;
+        } else if (num < 0) {
+            return "OOPS"
+        } else {
+            return 1;
+        }
+    }
+    return fibo(num);
 };
 
 // Do not edit below this line
