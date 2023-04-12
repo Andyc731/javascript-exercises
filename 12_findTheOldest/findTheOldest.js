c91ec6c1b9e6152372b645500e90663807c97d82
Andy <andychc731@gmail.com>
2023-04-12 13:36:22 -0600

    Add solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((accum, current) => {
+        if (age(accum.yearOfBirth, accum.yearOfDeath) > age(current.yearOfBirth, current.yearOfDeath)) {
+            return accum;
+        } else {
+            return current;
+        }
+    }, {})
 };
+
+function age(birth, death) {
+    if(!death) {
+        return new Date().getFullYear() - birth;
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
