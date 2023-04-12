90697864a1a6cb59591b3074e3fb41e5262c46ca
Andy <andychc731@gmail.com>
2023-04-12 11:14:00 -0600

    Add solutions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    str = str.replaceAll(" ", "");
+    str = str.toLowerCase();
+    let newStr = "";
+    for (i = str.length - 1; i >= 0; i--) {
+        newStr = newStr + str[i];
+    }
+    if (newStr === str) {
+        return true;
+    } else {
+        return false;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
